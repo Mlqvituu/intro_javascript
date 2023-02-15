1504c5105ef4565891e8b8300eadb6bd0ec44943
Mlqvituu <119640560+Mlqvituu@users.noreply.github.com>
2023-02-15 17:30:13 -0300

    variáveis

--- exemplos/000_javascript.js
+++ exemplos/000_javascript.js
@@ -1,1 +1,30 @@
+// o comando abaixo apresenta uma mensagem na tela
 console.log("Olá estou programando em javascript");
+
+/*
+   comentário que ocupa mais
+   de uma linha
+   */
+
+   // criação de variável do tipo texto
+   let nome = 'Victor hugo costa da silva';
+   // criação de variável do tipo numérico (inteiro)
+   let idade = 17;
+   // criação de variável do tipo numérico (decimal)
+   let altura = 1.67;
+   // criação de variável do tipo verdadeiro/falso
+   let trabalha = true; // true = verdadeiro  | false = falso
+   // exemplos de variável de nome composto 
+   let corFavorita = 'azul';
+   // exemplo de variável do tipo lista de valores
+   let cores_favoritas = ['azul', 'preto', 'vermelho'];
+   // exemplo de variável que armazena um objeto
+   let endereço ={
+       'logradouro'       : 'Rua',
+       'nome_logradouro'  : 'brasil',
+       'número'           : '22 A',
+       'bairro'           : 'ibirapuera',
+       'cidade'           :  'tupã'
+   };
+   let covid;
+   let dengue = null;
