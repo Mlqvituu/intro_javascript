c8177bfadc5a22e8b3ceff095880d21d69084917
Mlqvituu <119640560+Mlqvituu@users.noreply.github.com>
2023-04-19 19:46:38 -0300

    atv
    atv

--- exemplos/009-atividade.js
+++ exemplos/009-atividade.js
@@ -6,4 +6,17 @@
     A notas digitadas devem estar entre 0 e 10!
     */
    
-    
+    const entrada = require('prompt-sync')();
+
+    const nota1 = parseFloat(entrada('Digite sua nota 1:  '));
+    const nota2 = parseFloat(entrada('Digite sua nota 2:  '));
+    if (nota1 >= 0 && nota1 <= 10 || nota2 >= 0 && nota1 <= 10){
+        const media = (nota1 + nota2) /2;
+
+        if (media >= 7) {
+            console.log('aprovado');
+        } else {
+            console.log('reprovado');}
+        } else {
+            console.log('Nota invalida, so pode nota de 0 a 10')
+        }
