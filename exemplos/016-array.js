c43e8e7b96ed69253c436a11e98d714cac3fd222
Mlqvituu <119640560+Mlqvituu@users.noreply.github.com>
2023-05-03 17:28:54 -0300

    array
    array

---
+++ exemplos/016-array.js
@@ -0,0 +1,21 @@
+
+// Array
+
+/*
+let aluno1 = 'Tomaz trajano';
+let aluno2 = 'Raissa Rebeca';
+let aluno3 = 'Carlos Caetano';
+let aluno4 = 'Helna Harris';
+
+console.log('Lista de alunos:');
+console.log('- ' + aluno1);
+console.log('- ' + aluno2);
+console.log('- ' + aluno3);
+console.log('- ' + aluno4);
+*/
+
+let alunos = ['Tomaz trajano', 'Raissa Rebeca', 'Carlos Caetano', 'Helna Harris'];
+
+console.log(alunos);
+
+
